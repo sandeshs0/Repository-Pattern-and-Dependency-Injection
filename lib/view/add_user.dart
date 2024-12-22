b6e6d65271e7f3c45c5c040c8f9c933cacf13b70
sandeshs0 <hellosandesh0@gmail.com>
2024-12-22 20:49:50 +0545

    add user with getIt

--- lib/view/add_user.dart
+++ lib/view/add_user.dart
@@ -1,3 +1,14 @@
-void main(){
+import 'package:di_repository/di/di.dart';
+import 'package:di_repository/model/user.dart';
+import 'package:di_repository/repository/user_repository.dart';
 
+void main() {
+  initModule();
+  User user = User(id: 4, name: 'Ana de Armas', age: 30);
+
+  UserRepository userRepository = getIt<UserRepository>();
+  userRepository.addUser(user);
+
+// display all users
+  print(userRepository.getAllUsers());
 }
